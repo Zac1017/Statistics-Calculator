b8548c5b481ae1ed330470097ce9ff382db3edc4
Zac Engracia <9964262@bedfordnhk12.net>
2026-08-29 01:02:00 +0000

    Implement input handling and statistical calculations in Main and Statistics classes

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,23 @@
+import java.util.*;
+
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner scanner = new Scanner(System.in);
+
+        List<Double> list = new ArrayList<>();
+
+        while (true) {
+            String input = scanner.nextLine();
+            
+            if (input.contains(" ")) {
+                break;
+            }
+
+            double number = Double.parseDouble(input);
+            list.add(number);
+        }
+
+        Statistics stats = new Statistics(list);
+        System.out.println(stats.median());
     }
 }
--- src/Statistics.java
+++ src/Statistics.java
@@ -1,5 +1,39 @@
+import java.util.*;
+
 public class Statistics {
-    public Statistics() {
-        
+    List<Double> unsortedList;
+    List<Double> list;
+    double size;
+    public Statistics(List<Double> list) {
+        this.unsortedList = list;
+        this.list = sort();
+        this.size = list.size();
+    }
+    public double mean(){
+        double mean = 0;
+        for (double point : list) {
+            mean += point;
+        }
+        return mean / size;
+    }
+
+    public double median(){
+        if (size % 2 == 1) {
+            return list.get(middle);
+        }
+        return (list.get(middle - 1) + list.get(middle)) / 2;
+    }
+
+    public double mode(){
+        return 0.0;
+    }
+
+    public void sort(){
+      
     }
 }
+
+/*
+mean, median, mode, range, IQR, standard deviation, q1, q3, sum, count, z-scores
+five number summary -> min, q1, median, q3, max
+ */
