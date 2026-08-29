47a2c3ddf9880c72d65b25665b2a48d85dab39fa
Zac Engracia <9964262@bedfordnhk12.net>
2026-08-29 01:38:49 +0000

    Refactor Statistics class: change size type to int, implement mode, sort, range, q1, q3, and IQR methods

--- src/Statistics.java
+++ src/Statistics.java
@@ -3,7 +3,7 @@ import java.util.*;
 public class Statistics {
     List<Double> unsortedList;
     List<Double> list;
-    double size;
+    int size;
     public Statistics(List<Double> list) {
         this.unsortedList = list;
         this.list = sort();
@@ -18,6 +18,8 @@ public class Statistics {
     }
 
     public double median(){
+        int middle = size / 2;
+
         if (size % 2 == 1) {
             return list.get(middle);
         }
@@ -25,11 +27,68 @@ public class Statistics {
     }
 
     public double mode(){
-        return 0.0;
+        double topRep = 0;
+        double mode = list.get(0);
+
+        for (double point : list){
+            int rep = 0;
+
+            for (double other : list){
+                if (point == other) {
+                    rep++;
+                }
+            }
+
+            if (rep > topRep) {
+                topRep = rep;
+                mode = point;
+            }
+        }
+        
+        if (topRep <= 1) {
+            return Double.NaN;
+        }
+        return mode;
     }
 
-    public void sort(){
-      
+    public List<Double> sort(){
+      list = new ArrayList<>(unsortedList);
+      list.sort(null);
+      return list;
+    }
+
+    public double range(){
+        return list.get(list.size() - 1) - list.get(0);
+    }
+
+    public double q1(){
+        int middle = size / 2;
+        List<Double> firstHalf = list.subList(0, middle);
+
+        int halfMiddle = firstHalf.size() / 2;
+
+        if (firstHalf.size() % 2 == 1) {
+            return firstHalf.get(halfMiddle);
+        }
+
+        return (firstHalf.get(halfMiddle - 1) + firstHalf.get(halfMiddle)) / 2;
+    }
+
+    public double q3(){
+        int middle = size / 2;
+        List<Double> secondHalf = list.subList(middle, size);
+
+        int halfMiddle = secondHalf.size() / 2;
+
+        if (secondHalf.size() % 2 == 1) {
+            return secondHalf.get(halfMiddle);
+        }
+
+        return (secondHalf.get(halfMiddle - 1) + secondHalf.get(halfMiddle)) / 2;
+    }
+
+    public double iqr(){
+        return q3() - q1();
     }
 }
 
